d17462d099a9a148aeb4ec1b7d567237b38cdfb4
Daniel Yount <TheDanYount@gmail.com>
2024-10-04 21:30:03 +0000

    Created page background #1

--- client/src/components/Cookbook.tsx
+++ client/src/components/Cookbook.tsx
@@ -1,3 +1,5 @@
+import { Page } from './Page';
+
 type Props = {
   style: string;
   title: string;
@@ -10,10 +12,14 @@ export function Cookbook({ style, title }: Props) {
     <div className="flex w-[588px] m-[60px]">
       <div
         className="w-[294px] h-[372px] rounded-l-[6px] pt-[13px] pl-[13px]"
-        style={{ backgroundColor: style }}></div>
+        style={{ backgroundColor: style }}>
+        <Page left={true} />
+      </div>
       <div
         className="w-[294px] h-[372px] rounded-r-[6px] pt-[13px]"
-        style={{ backgroundColor: style }}></div>
+        style={{ backgroundColor: style }}>
+        <Page left={false} />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Page.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  left: boolean;
+};
+
+export function Page({ left }: Props) {
+  return (
+    <div
+      className={`w-[281px] h-[346px] bg-gradient-to-r
+        ${left ? 'from-[#FFE8AA]' : 'from-[#A89971]'}
+        ${left ? 'from-90%' : 'from-0%'}
+        ${left ? 'to-[#A89971]' : 'to-[#FFE8AA]'}
+        ${left || 'to-10%'}`}></div>
+  );
+}
