8b684723621e97ded4d79fd99d50dfa4a5b80f9a
Daniel Yount <TheDanYount@gmail.com>
2024-10-05 18:29:28 +0000

    Made image input an image #1
    Added multer to make this work

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,11 @@
 import 'dotenv/config';
 import express from 'express';
 import pg from 'pg';
-import { ClientError, errorMiddleware } from './lib/index.js';
+import {
+  ClientError,
+  errorMiddleware,
+  uploadsMiddlewareRecipes,
+} from './lib/index.js';
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
@@ -22,55 +26,60 @@ app.use(express.static(reactStaticDir));
 app.use(express.static(uploadsStaticDir));
 app.use(express.json());
 
-app.post('/api/create-recipe', async (req, res, next) => {
-  try {
-    const {
-      cookbookId,
-      title,
-      imageUrl,
-      isFavorite,
-      ingredients,
-      directions,
-      notes,
-      order,
-      length,
-      isPublic,
-    } = req.body;
-    if (!cookbookId) throw new ClientError(400, 'cookbookId is required');
-    if (!title) throw new ClientError(400, 'title is required');
-    if (!(typeof imageUrl === 'string'))
-      throw new ClientError(400, 'imageUrl is required');
-    if (!(typeof isFavorite === 'boolean'))
-      throw new ClientError(400, 'isFavorite is required');
-    if (!ingredients) throw new ClientError(400, 'ingredients is required');
-    if (!directions) throw new ClientError(400, 'directions is required');
-    if (!notes) throw new ClientError(400, 'notes is required');
-    if (!order) throw new ClientError(400, 'order is required');
-    if (!length) throw new ClientError(400, 'length is required');
-    if (!(typeof isPublic === 'boolean'))
-      throw new ClientError(400, 'isPublic is required');
-    const sql = `
+app.post(
+  '/api/create-recipe',
+  uploadsMiddlewareRecipes.single('image'),
+  async (req, res, next) => {
+    try {
+      if (!req.file) throw new ClientError(400, 'no file field in request');
+      const imageUrl = `/images/recipe-images/${req.file.filename}`;
+      const { title, ingredients, directions, notes } = req.body;
+      // Remove once form is updated
+      const isFavorite = false;
+      // Remove once form is updated
+      const isPublic = false;
+      // Remove once form is updated
+      const length = 1;
+      // Remove once form is updated
+      const order = 1;
+      // Remove once form is updated
+      const cookbookId = 1;
+      if (!cookbookId) throw new ClientError(400, 'cookbookId is required');
+      if (!title) throw new ClientError(400, 'title is required');
+      if (!(typeof imageUrl === 'string'))
+        throw new ClientError(400, 'imageUrl is required');
+      if (!(typeof isFavorite === 'boolean'))
+        throw new ClientError(400, 'isFavorite is required');
+      if (!ingredients) throw new ClientError(400, 'ingredients is required');
+      if (!directions) throw new ClientError(400, 'directions is required');
+      if (!notes) throw new ClientError(400, 'notes is required');
+      if (!order) throw new ClientError(400, 'order is required');
+      if (!length) throw new ClientError(400, 'length is required');
+      if (!(typeof isPublic === 'boolean'))
+        throw new ClientError(400, 'isPublic is required');
+      const sql = `
     insert into "recipes" ("cookbookId", "title", "imageUrl", "isFavorite", "ingredients", "directions", "notes", "order", "length", "isPublic")
     values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
     returning *;
     `;
-    const result = await db.query(sql, [
-      cookbookId,
-      title,
-      imageUrl,
-      isFavorite,
-      ingredients,
-      directions,
-      notes,
-      order,
-      length,
-      isPublic,
-    ]);
-    res.status(201).json(result.rows[0]);
-  } catch (err) {
-    next(err);
+      const result = await db.query(sql, [
+        cookbookId,
+        title,
+        imageUrl,
+        isFavorite,
+        ingredients,
+        directions,
+        notes,
+        order,
+        length,
+        isPublic,
+      ]);
+      res.status(201).json(result.rows[0]);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 /*
  * Handles paths that aren't handled by any other route handler.
