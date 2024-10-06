5b578a190334660c46042cef7d0d24f6af14aa03
Daniel Yount <TheDanYount@gmail.com>
2024-10-06 20:15:04 +0000

    Added currentPage state #1

--- client/src/components/Cookbook.tsx
+++ client/src/components/Cookbook.tsx
@@ -11,23 +11,42 @@ type PageData = {
   data: object;
 };
 
+const dummyPagesForDevelopment = [
+  { type: 'title', data: {} },
+  { type: 'toc', data: {} },
+  { type: 'edit', data: {} },
+];
+
 export function Cookbook({ style, title }: Props) {
-  const [pages, setPages] = useState<PageData[]>([]);
+  const [pages, setPages] = useState<PageData[]>([...dummyPagesForDevelopment]);
+  const [currentPage, setCurrentPage] = useState(1);
   // The following is just here to calm my linter during development
   console.log(title, 'title');
   console.log(pages, 'pages');
   console.log(setPages, 'setPages');
+
+  function handlePageTurn(number) {
+    const newPageNum = currentPage + number;
+    if (newPageNum >= 1 && newPageNum <= pages.length) {
+      setCurrentPage(currentPage + number);
+    }
+  }
+
   return (
     <div className="flex w-[588px] m-[60px]">
       <div
         className="w-[294px] h-[372px] rounded-l-[6px] pt-[13px] pl-[13px]"
         style={{ backgroundColor: style }}>
-        <Page left={true} />
+        <Page left={true} onPageTurn={handlePageTurn} pageNum={currentPage} />
       </div>
       <div
         className="w-[294px] h-[372px] rounded-r-[6px] pt-[13px]"
         style={{ backgroundColor: style }}>
-        <Page left={false} />
+        <Page
+          left={false}
+          onPageTurn={handlePageTurn}
+          pageNum={currentPage + 1}
+        />
       </div>
     </div>
   );
--- client/src/components/Page.tsx
+++ client/src/components/Page.tsx
@@ -2,17 +2,24 @@ import { RecipeForm } from './RecipeForm';
 
 type Props = {
   left: boolean;
+  onPageTurn: (number) => void;
+  pageNum: number;
 };
 
-export function Page({ left }: Props) {
+export function Page({ left, onPageTurn, pageNum }: Props) {
   return (
     <div
-      className={`w-[281px] h-[346px] bg-gradient-to-r
+      className={`flex flex-col justify-between w-[281px] h-[346px] bg-gradient-to-r
         ${left ? 'from-[#FFE8AA]' : 'from-[#A89971]'}
         ${left ? 'from-90%' : 'from-0%'}
         ${left ? 'to-[#A89971]' : 'to-[#FFE8AA]'}
         ${left || 'to-10%'}`}>
       {left && <RecipeForm />}
+      <div className="flex text-xs">
+        <button onClick={() => onPageTurn(-1)}>Back</button>
+        <p className="mx-2">{pageNum}</p>
+        <button onClick={() => onPageTurn(1)}>Next</button>
+      </div>
     </div>
   );
 }
