80f3ad5c7551a783cba9448886e0208d1052e50d
Daniel Yount <TheDanYount@gmail.com>
2024-10-15 03:39:35 +0000

    edited ToC hover to include page num

--- client/src/components/ToCEntry.tsx
+++ client/src/components/ToCEntry.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { RxCaretSort } from 'react-icons/rx';
-import { FaPencilAlt } from 'react-icons/fa';
+import { FaPencilAlt, FaTrash } from 'react-icons/fa';
 import { convertRecipeToForm } from '../lib/page-scaffolding';
 import { PageData } from './Cookbook';
 
@@ -46,13 +46,16 @@ export function ToCEntry({
       data-length={length}
       data-placementonpage={placementOnPage}
       onPointerMove={onPointerMove}>
-      <div className="absolute flex justify-between items-start w-full">
-        <div className="flex hover:scale-110 hover:cursor-pointer">
+      <div className="absolute children-hover flex justify-between items-start w-full">
+        <div className="flex hover:cursor-pointer">
           <div className="text-base" onPointerDown={onPointerDown}>
             <RxCaretSort />
           </div>
           <div className="my-[2px]" onClick={handleEdit}>
             <FaPencilAlt />
+          </div>
+          <div className="my-[2px]" onClick={handleEdit}>
+            <FaTrash />
           </div>
           <p className="select-none" onClick={handleRecipeNavigation}>
             {text}
