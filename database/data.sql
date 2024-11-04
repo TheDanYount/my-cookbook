29eeb8a7abf1cb22e2d997a7681ea2a0ae2848c0
Daniel Yount <TheDanYount@gmail.com>
2024-11-04 18:57:01 +0000

    Polish
    Handled duplicate username checking w/ a unique key, eliminating the need for a separate db query.

--- database/data.sql
+++ database/data.sql
@@ -9,9 +9,3 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
-
-insert into "users" ("photoUrl", "username", "password", "email", "firstName", "lastName", "style")
-values ('test', 'dan', 'danpass', 'dan@dan.dan', 'dan', 'yount', '#C45056');
-
-insert into "cookbooks" ("userId", "style", "title", "isPublic")
-values (1, '#4C301E', 'My First Cookbook', false);
--- server/server.ts
+++ server/server.ts
@@ -47,14 +47,6 @@ app.post(
         lastName,
         style,
       } = req.body;
-      const usernameCheckSql = `
-      select *
-      from "users"
-      where "username" = $1
-      `;
-      const usernameCheckResult = await db.query(usernameCheckSql, [username]);
-      if (usernameCheckResult.rows[0])
-        throw new ClientError(400, `username already exists`);
       const hashedPassword = await argon2.hash(password);
       const photoUrl = `/images/recipe-images/${req?.file?.filename}`;
       let sql;
